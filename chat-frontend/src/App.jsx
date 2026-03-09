6e1f93c59f3a3b8303ae9f57fa6a740bd9668701
Chethan <chethand5515@gmail.com>
2026-03-09 23:49:29 +0530

    "Fix auth page layout and resolve MongoDB connection error"

--- chat-frontend/src/App.jsx
+++ chat-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ChatApp from "./chatApp";
-
+//App
 function App() {
 
   const [page, setPage] = useState("register");
